97f1039a7afefcb530e58eb162b0b4aa607c8c49
AishwaryaAtos <127492201+AishwaryaAtos@users.noreply.github.com>
2023-06-29 14:01:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const github = require('@actions/github');
 try {
   // `number_1 and number_2` input defined in action metadata file
   const num_1 = core.getInput('number_1');
-  console.log(`First number ${num_1}!`);
+  console.log(`First number ${num_1}`);
   const num_2 = core.getInput('number_2');
-  console.log(`Second number ${num_2}!`);
-  const add = int(num_1) + int(num_2)
-  console.log(`Addition of two numbers ${add}!`);
-  core.setOutput("sum", int(add));
+  console.log(`Second number ${num_2}`);
+  const add = num_1 + num_2
+  console.log(`Addition of two numbers ${add}`);
+  core.setOutput("sum", add);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
